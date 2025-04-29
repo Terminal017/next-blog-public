1f8af1c26d1d04947ec943620ccd83e204dfe639
Star Trail <1404256812@qq.com>
2025-04-29 15:46:45 +0800

    增加not-found页面

---
+++ src/app/not-found.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-row justify-center items-center gap-6 mt-8 mb-8">
+      <h2 className="text-2xl text-on-background">404 - Not Found</h2>
+      <div className="w-px h-10 bg-outline-v/80"></div>
+      <p className="text-xl text-on-background">你来到了未知之地</p>
+    </div>
+  );
+}
