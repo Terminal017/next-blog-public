82e6bd6f80fb54028b19e4d300e2e58824ca29d1
Star Trail <1404256812@qq.com>
2025-05-15 08:49:11 +0800

    优化markdown解析方式
    采用MDX,next-mdx-remote-client,remark,rehype替换marked，提供更高的拓展性

--- src/app/article/[slug]/page.jsx
+++ src/app/article/[slug]/page.jsx
@@ -1,9 +1,13 @@
 import fs from "fs/promises";
 import path from "path";
-import { marked, Renderer } from "marked";
 import { notFound } from "next/navigation";
 import ArticleTOC from "@/ui/toc.jsx";
 import { article_map } from "../data";
+import { MDXRemote } from "next-mdx-remote-client/rsc";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkGfm from "remark-gfm";
+import remarkSlug from "remark-slug";
+import { visit } from "unist-util-visit";
 
 // gene用于生成动态路由的所有可用路由，返回值应该当是一个对象数组：{ id: string }[]。在构建时会自动调用
 export async function generateStaticParams() {
@@ -11,9 +15,9 @@ export async function generateStaticParams() {
   const files = await fs.readdir(articlesDirectory);
 
   return files
-    .filter((file) => file.endsWith(".md"))
+    .filter((file) => file.endsWith(".mdx"))
     .map((file) => ({
-      slug: file.replace(/\.md$/, ""),
+      slug: file.replace(/\.mdx$/, ""),
     }));
 }
 
@@ -29,14 +33,13 @@ export async function generateMetadata({ params }) {
   };
 }
 
-// params为路由参数：{ slug: "hello-world" }
 export default async function Page({ params }) {
   const { slug } = await params;
   const filePath = path.join(
     process.cwd(),
     "content",
     "articles",
-    `${slug}.md`
+    `${slug}.mdx`
   );
   let content = "";
 
@@ -47,36 +50,43 @@ export default async function Page({ params }) {
     notFound();
   }
 
-  //Markdown转换为HTML
   const headings = [];
+  function remarkExtractHeadings(headings) {
+    return () => (tree) => {
+      visit(tree, "heading", (node) => {
+        if (node.depth === 3) {
+          const textNode = node.children.find((child) => child.type === "text");
+          if (textNode) {
+            const text = textNode.value;
+            const id = node.data?.id;
+            if (id) {
+              headings.push({ text, id });
+            }
+          }
+        }
+      });
+    };
+  }
 
-  const renderer = new Renderer();
-  renderer.heading = (token) => {
-    const { text, depth } = token;
-    if (depth === 3) {
-      // 生成 id 并记录
-      const id = text.toLowerCase().replace(/\s+/g, "-");
-      headings.push({ text, id });
-      return `<h3 id="${id}">${text}</h3>`;
-    }
-    //其他暂时保持默认
-    return `<h${depth}>${text}</h${depth}>`;
+  const options = {
+    mdxOptions: {
+      remarkPlugins: [
+        remarkFrontmatter,
+        remarkGfm,
+        remarkSlug,
+        remarkExtractHeadings(headings),
+      ],
+    },
+    parseFrontmatter: true, // 是否解析 frontmatter，默认为 false
   };
 
-  marked.use({ renderer });
-  marked.setOptions({
-    gfm: true, //启用GFM规范
-    breaks: true, //启用换行符转换为br
-    headerIds: false,
-  });
-  const html = marked.parse(content);
+  const components = {};
 
   return (
     <>
-      <article
-        className="article-container"
-        dangerouslySetInnerHTML={{ __html: html }}
-      />
+      <article className="article-container">
+        <MDXRemote source={content} options={options} components={components} />
+      </article>
       <ArticleTOC headings={headings} />
     </>
   );
